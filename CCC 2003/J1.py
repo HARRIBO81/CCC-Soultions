5bdd567aad879e05c3e7f62d2bbd9c0d8ef54158
HARRIBO81 <58157787+HARRIBO81@users.noreply.github.com>
2021-04-24 03:28:21 -0400

    Create J1.py

---
+++ CCC 2003/J1.py
@@ -0,0 +1,12 @@
+t = int(input())
+s = int(input())
+h = int(input())
+
+for i in range(t):
+    print("*"+" "*s+"*"+" "*s+"*")
+
+print("*"+"*"*s+"*"+"*"*s+"*")
+
+
+for b in range(h):
+    print(" "*s+" "+"*")
