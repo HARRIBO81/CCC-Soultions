2a07de6c7219f863f701f38f876cb80b259b15d3
HARRIBO81 <58157787+HARRIBO81@users.noreply.github.com>
2021-04-25 17:40:00 -0400

    Create S2.java

---
+++ CCC 2019/S2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+    static boolean isPrime(int num)
+    {
+        if (num == 2)
+            return true;
+        if (num < 2 || num % 2 == 0)
+            return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+       int n = readInt();
+       for(int i =0;i<n;i++){
+           int number = readInt();
+           int left = number;
+           int right = number;
+           while(!isPrime(left) || !isPrime(right)){
+               left --;
+               right ++;
+           }
+           System.out.println(left + " " + right);
+       }
+    }
+}
