c667caad5707a149965852ff86bd65e1f3538308
HARRIBO81 <58157787+HARRIBO81@users.noreply.github.com>
2021-04-25 03:17:42 -0400

    Create J1.py

---
+++ CCC 2018/J1.py
@@ -0,0 +1,20 @@
+x = int(input())
+y = int(input())
+n = int(input())
+k = int(input())
+
+
+if x == 8 and k == 9 and y == n:
+    print("ignore")
+
+elif x == 9 and k == 8 and y == n:
+    print("ignore")
+
+elif x == 9 and k == 9 and y == n:
+    print("ignore")
+    
+elif x == 8 and k == 8 and y == n:
+    print("ignore")
+
+else:
+    print("answer")
