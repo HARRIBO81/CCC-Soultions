98f938818d00354f78e9762767fcdc3ad1061f9e
HARRIBO81 <58157787+HARRIBO81@users.noreply.github.com>
2021-04-25 02:10:41 -0400

    Create J1.py

---
+++ CCC 2015/J1.py
@@ -0,0 +1,20 @@
+month = int(input())
+day = int(input())
+
+if month > 2:
+    print("After")
+
+elif month == 2 and day == 18:
+    print("Special")
+
+
+elif month >= 2 and day > 18:
+    print("After")
+
+
+
+elif month >= 2 and day < 18:
+    print("Before")
+
+else:
+    print("Before")
