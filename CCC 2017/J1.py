4a217dc20504c1fb99f8ebaaa1aa2cd6642ecb22
HARRIBO81 <58157787+HARRIBO81@users.noreply.github.com>
2021-04-25 03:07:59 -0400

    Create J1.py

---
+++ CCC 2017/J1.py
@@ -0,0 +1,14 @@
+n = int(input())
+k = int(input())
+
+
+if n > 0:
+    if k < 0:
+        print("4")
+    else:
+        print("1")
+if n < 0:
+    if k < 0:
+        print("3")
+    else:
+        print("2")
